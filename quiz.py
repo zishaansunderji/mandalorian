9fd9a016c5a321f3d998127429e24add6e152119
zishaansunderji <40409234+zishaansunderji@users.noreply.github.com>
2021-01-04 09:46:11 -0500

    Update quiz.py
    Removed win32 sys check

--- quiz.py
+++ quiz.py
@@ -4,9 +4,6 @@ from selenium import webdriver
 class Quiz:
     def __init__(self, url, combination_of_choices,
             chrome_path=r"driver\chromerdriver.exe"):
-        if sys.platform != "win32":
-            print("Sorry this operating system in not yet supported...")
-            exit()
         self.url = url
         self.combination_of_choices = combination_of_choices
         self.chrome_path = chrome_path
